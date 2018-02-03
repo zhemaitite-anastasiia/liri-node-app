4b7a4510f3b0692858350d85f2f9276049db5f1f
AnastasiiaZhemaitite <a.zhemaitite@gmail.com>
2018-02-02 20:17:32 -0600

    my-tweets function added

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # liri-node-app
+
+LIRI-Bot 
+
+Project Description:
+
+Liri is like iPhone’s Siri. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI is a command line node app that takes in parameters and gives you back data.
+
+Liri has the ability to retrieve the following information:
+
+- Your tweets from your Twitter account
+- Searches songs on Spotify
+- Searches movies in OMDB
+
+Technologies Used
+
+Node.js
+Javascript
+Package.JSON
+NPM Modules
--- liri.js
+++ liri.js
@@ -1,9 +1,9 @@
+var fs = require('fs');
 
 
 //Grabbing data from keys.js
-var fs = require('fs');
-
 var Keys = require('./keys.js');
+//installing libraries and making requests
 var request = require('request');
 var twitter = require('twitter');
 var spotify = require('spotify');
@@ -15,33 +15,66 @@ var nodeArgv = process.argv;
 var command = process.argv[2];
 
 //Song or movie
-switch (command){
-	//handle the my-tweets command
-	case 'my-tweets':
-	myTweets();
-	break;
+switch (command) {
+    //handle the my-tweets command
+    case 'my-tweets':
+        myTweets();
+        break;
 
+        //handle the my-spotify command
+    case 'my-spotify':
+        mySpotify(nodeArgv);
+        break;
 
-    //handle the my-spotify command
-	case 'my-spotify':
-	mySpotify(nodeArgv);
-	break;
+        //handle the movie command
+    case 'movie-this':
+        movieThis(nodeArgv);
+        break;
 
-	//handle the movie command
-	case 'movie-this':
-	movieThis(nodeArgv);
-	break;
+        //handle do-what-it-says command
+    case 'do-what-it-says';
+    doWhatItSays();
+    break;
+    //when command is not valid
+    default:
+        console.log("Command not valid! Please try again!");
 
-	//handle do-what-it-says command
-	case 'do-what-it-says';
-	doWhatItSays();
-	break;
+}
 
-	default:
-	console.log("Command not valid! Please try again!");
+//if the my-tweets function received
+function myTweets() {
 
+    //setting up credentials object for Twitter access
+    var client = new Twitter({
+        consumer_key: twitterKeys.consumer_key,
+        consumer_secret: twitterKeys.consumer_secret,
+        access_token_key: twitterKeys.access_token_key,
+        access_token_secret: twitterKeys.access_token_secret
+    })
+
+    client.get('statuses/user_timeline', function(error, tweets, response) {
+        if (error) {
+            console.log(error);
+            return;
+            // going through tweets, to a maximum of 20 but no mmore tweets than on account
+            for (var i = 0; i < Math.min(20, tweets.length); i++) {
+                output += tweets[i].created_at + "\n" + tweets[i].text + "\n";
+
+            }
+        }
+    })
 }
 
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
